7ae24672edad95673b03f1264dffa75764d1a46e
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-11-22 17:20:39 +0530

    first commit

---
+++ todo-project/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics,permissions
+from .serializers import TodoSerializer
+from todo.models import Todo
+
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user,datecompleted__isnull=False).order_by('-datecompleted')
+
